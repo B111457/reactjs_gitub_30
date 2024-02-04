b42214ceb41d46ecdae231c2334a7b7263a8ee2a
Praveen Raj <kalepraveenraj@gmail.com>
2024-02-04 13:24:40 +0530

    header_footer

---
+++ 03-react-header-footer/src/Header.js
@@ -0,0 +1,13 @@
+import react from "react";
+import './Header.css';
+
+
+function Header(){
+    return (
+        <div>
+            <h1>JavaExpress</h1>
+            <h5>FullStack Web Technologies</h5>
+        </div>
+    )
+}
+export default Header;

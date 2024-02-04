76888f1f468e3627c125a9d0ff03884662a40e7d
Praveen Raj <kalepraveenraj@gmail.com>
2024-02-04 12:59:54 +0530

    functionallevelcomponent

---
+++ 02-react-functionallevelcomponent/src/First.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function First()
+{
+    return(
+        <div>
+            <h1>Wellcome to functional level COmponent</h1>
+            
+        </div>
+    );
+
+}
+
+
+export default First;

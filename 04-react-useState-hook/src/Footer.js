894961b75e36de7e287e69f0cf5487b8679fd582
Praveen Raj <kalepraveenraj@gmail.com>
2024-02-09 18:10:07 +0530

    use state hook

---
+++ 04-react-useState-hook/src/Footer.js
@@ -0,0 +1,12 @@
+import react from "react";
+import './Footer.css';
+
+
+function Footer(){
+    return (
+        <div>
+            <h5>copyright@JavaExpress.com</h5>
+        </div>
+    )
+}
+export default Footer;
